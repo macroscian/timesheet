afbdad10fbbe9ef294033bc54e0ac09b647a0959
Gavin Kelly <gavin.kelly@crick.ac.uk>
2020-11-08 21:40:07 +0000

    fix #002 Spent hours not updating
    recalc was not handling the 'spent' field.  Also the indentation was
    wrong, so all the whitespace has changed, but this really just adds
    one line to recalc, and wraps the spent field in a class.

--- record.php
+++ record.php
@@ -134,205 +134,208 @@ error_reporting(E_ALL);
       <button type="button" class="btn btn-primary" onClick="submit()">Submit</button>
     </div>
     <script>
-      var hours_per_day = 36.0/5;
-      var db_state={};
-      var detail_week=""; // slot to remember which week we want to split into days
-      get_timesheet({});
-      d3.select("#generic").on("change", toggle_generic);
-      function get_timesheet(range) {
-      range.Bioinformatician = "<?php echo $_GET["id"]; ?>";
-      d3.json('get_time.php', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify(range)
-      })
-      .then(hours_so_far);
-      }
-      function hours_so_far(data) {
-      var hashes={hashes:active_projects.map(x => x.Hash), id:"<?php echo $_GET["id"]; ?>"};
-      d3.json('project_hours.php', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify(hashes)
-      }).then(d => update_view(data, d));
-      }
-      function update_view(data, hours) {
-      db_state = data;
-      detail_week = (db_state.hasOwnProperty('day'))?d3.timeFormat('%GW%V')(new Date(db_state.start)):detail_week;
-      let db_entries = Array.from(d3.rollup(data.entries, aggregate_projs, d => d.Hash), ([key, value]) => value);
-      delete db_state.entries;
-      d3.select("#catchup").html(parse_time_label(db_state));
-      d3.select("#catchuphours").text(db_state.hours_needed);
-      active_projects = JSON.parse(JSON.stringify(base_projects)); 
-      db_entries.forEach(proj => {
-      let ind = active_projects.findIndex(p => p.Hash==proj.Hash);
-      if (ind != -1) {
-      let my_hours = proj.Hours;
-      let my_note = proj.Note;
-      Object.assign(proj, active_projects[ind]);
-      proj.Hours = my_hours;
-      proj.Note = my_note;
-      active_projects.splice(ind, 1);
-      }
-      proj.activated=true;
-      proj.orig_activated = proj.activated;
-      proj.orig_hours = proj.Hours;
-      proj.orig_note = proj.Note;
-      proj.fixed=true;
-      proj.default_time=proj.Hours;
-      });
-      active_projects=db_entries.concat(active_projects);
-      active_projects.forEach(proj => {
-      let ind = hours.findIndex(h => h.Hash==proj.Hash);
-      proj.spent = (ind == -1)?0:(hours[ind].hours);
-      });
-      update_rows(active_projects);
-      toggle_generic();
-      }
-      function update_rows(data) {
-      var rows = d3.select("#projects").selectAll("tr").data(active_projects, d => d.Hash);
-      rows.enter().append("tr")
-      .html(generate_row_from_data);
-      rows.exit().remove();
-      rows.classed("text-muted", d => !d.activated);
-      d3.selectAll(".hour_select").on("change", handle_select);
-      d3.selectAll(".hour_input").on("change", handle_input);
-      d3.selectAll(".hour_increment").on("click", handle_increment);
-      d3.selectAll(".hour_note").on("change", handle_note);
-      recalc();
-      d3.select("#nav").classed("bg-warning", false);
-      }
-      function toggle_generic() {
-      d3.select("#projects").selectAll("tr")
-      .style("display", d => (d3.select(this).property("checked") || d.activated || ! d.generic)?null:"none");
-      }
-      function handle_hash(hash) {
-      d3.json('get_time.php', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({Hash: hash})
-      })
-      .then(function(hash_row) {
-      if (hash_row) {
-      hash_row.Hours = 0;
-	  hash_row.Note = "";
-	  hash_row.spent=0;
-      active_projects.push(hash_row);
-      update_rows(active_projects);
-      }
-      });
-      }
-      function submit() {
-      db_state.entries = d3.select("#projects").selectAll("tr").data()
-      .filter(d => d.activated)
-      .map(proj =>  {
-      proj.Date=db_state.recorddate;
-      return(proj);});
-      fetch('submit_entries.php', {
-      method: 'POST', // or 'PUT'
-      headers: {
-      'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(db_state)
-      });
-      get_timesheet({});
-      }
-      function aggregate_projs(v) {
-      res = Object.assign({}, v[0]);
-      res.Hours = d3.sum(v, d=> d.Hours);
-      res.orig_hours = res.Hours;
-      return(res);
-      }
+     var hours_per_day = 36.0/5;
+     var db_state={};
+     var detail_week=""; // slot to remember which week we want to split into days
+     get_timesheet({});
+     d3.select("#generic").on("change", toggle_generic);
+     function get_timesheet(range) {
+	 range.Bioinformatician = "<?php echo $_GET["id"]; ?>";
+	 d3.json('get_time.php', {
+	     method: 'POST',
+	     headers: { 'Content-Type': 'application/json'},
+	     body: JSON.stringify(range)
+	 })
+	   .then(hours_so_far);
+     }
+     function hours_so_far(data) {
+	 var hashes={hashes:active_projects.map(x => x.Hash), id:"<?php echo $_GET["id"]; ?>"};
+	 d3.json('project_hours.php', {
+	     method: 'POST',
+	     headers: { 'Content-Type': 'application/json'},
+	     body: JSON.stringify(hashes)
+	 }).then(d => update_view(data, d));
+     }
+     function update_view(data, hours) {
+	 db_state = data;
+	 detail_week = (db_state.hasOwnProperty('day'))?d3.timeFormat('%GW%V')(new Date(db_state.start)):detail_week;
+	 let db_entries = Array.from(d3.rollup(data.entries, aggregate_projs, d => d.Hash), ([key, value]) => value);
+	 delete db_state.entries;
+	 d3.select("#catchup").html(parse_time_label(db_state));
+	 d3.select("#catchuphours").text(db_state.hours_needed);
+	 active_projects = JSON.parse(JSON.stringify(base_projects)); 
+	 db_entries.forEach(proj => {
+	     let ind = active_projects.findIndex(p => p.Hash==proj.Hash);
+	     if (ind != -1) {
+		 let my_hours = proj.Hours;
+		 let my_note = proj.Note;
+		 Object.assign(proj, active_projects[ind]);
+		 proj.Hours = my_hours;
+		 proj.Note = my_note;
+		 active_projects.splice(ind, 1);
+	     }
+	     proj.activated=true;
+	     proj.orig_activated = proj.activated;
+	     proj.orig_hours = proj.Hours;
+	     proj.orig_note = proj.Note;
+	     proj.fixed=true;
+	     proj.default_time=proj.Hours;
+	 });
+	 active_projects=db_entries.concat(active_projects);
+	 active_projects.forEach(proj => {
+	     let ind = hours.findIndex(h => h.Hash==proj.Hash);
+	     proj.spent = (ind == -1)?0:(hours[ind].hours);
+	 });
+	 update_rows(active_projects);
+	 toggle_generic();
+     }
+     function update_rows(data) {
+	 var rows = d3.select("#projects").selectAll("tr").data(active_projects, d => d.Hash);
+	 rows.enter().append("tr")
+	     .html(generate_row_from_data);
+	 rows.exit().remove();
+	 rows.classed("text-muted", d => !d.activated);
+	 d3.selectAll(".hour_select").on("change", handle_select);
+	 d3.selectAll(".hour_input").on("change", handle_input);
+	 d3.selectAll(".hour_increment").on("click", handle_increment);
+	 d3.selectAll(".hour_note").on("change", handle_note);
+	 recalc();
+	 d3.select("#nav").classed("bg-warning", false);
+     }
+     function toggle_generic() {
+	 d3.select("#projects").selectAll("tr")
+	   .style("display", d => (d3.select(this).property("checked") || d.activated || ! d.generic)?null:"none");
+     }
+     function handle_hash(hash) {
+	 d3.json('get_time.php', {
+	     method: 'POST',
+	     headers: { 'Content-Type': 'application/json'},
+	     body: JSON.stringify({Hash: hash})
+	 })
+	   .then(function(hash_row) {
+	       if (hash_row) {
+		   hash_row.Hours = 0;
+		   hash_row.Note = "";
+		   hash_row.spent=0;
+		   active_projects.push(hash_row);
+		   update_rows(active_projects);
+	       }
+	   });
+     }
+     function submit() {
+	 db_state.entries = d3.select("#projects").selectAll("tr").data()
+			      .filter(d => d.activated)
+			      .map(proj =>  {
+				  proj.Date=db_state.recorddate;
+				  return(proj);});
+	 fetch('submit_entries.php', {
+	     method: 'POST', // or 'PUT'
+	     headers: {
+		 'Content-Type': 'application/json',
+	     },
+	     body: JSON.stringify(db_state)
+	 });
+	 get_timesheet({});
+     }
+     function aggregate_projs(v) {
+	 res = Object.assign({}, v[0]);
+	 res.Hours = d3.sum(v, d=> d.Hours);
+	 res.orig_hours = res.Hours;
+	 return(res);
+     }
 
-      function parse_time_label(php_response) {
-      if (php_response.hasOwnProperty('day')) {
-      let d = d3.timeParse("%Y-%m-%d")(php_response.day);
-      db_state.hours_needed=hours_per_day;
-      return(d3.timeFormat("%a %e %b %Y")(d));
-      }
-      if (php_response.hasOwnProperty('week')) {
-      let d = d3.timeParse("%GW%V")(php_response.week);
-      db_state.hours_needed=hours_per_day * 5;
-      return(d3.timeFormat("week beginning %a %e %b %Y")(d));
-      }
-      if (php_response.hasOwnProperty('month')) {
-      let d = d3.timeParse("%GW%V")(php_response.month);
-      db_state.hours_needed=hours_per_day * 0;
-      return(d3.timeFormat("%B %Y")(d));
-      }
-      
-      }
-      function movetime(forward) {
-      var cur = new Date(db_state.start);
-      var nxt;
-      db_state.move = (forward)?"forward":"reverse";
-      if (!unsaved || confirm("You're navigating away from unsaved changes")) {
-      get_timesheet(db_state);
-      }
-      }
-      function generate_row_from_data(d,i) {
-      var html = '<td><input type="checkbox" class="form-check-input hour hour_select" autocomplete="off" ' + (d.activated?"checked":"") + '>' + 
-      '<span class="hour_increment">' + d.Project + (d.codekey?(" ("+d.codekey+")"):"") + '</span>' +
-      "</td><td>" + d.Scientist +
-      "</td><td>" + d.Lab +
-      '</td><td><input size="5" type="number" min="0" max="168" step="' + (0.1) +'" class="hour hour_input" value="' + d.Hours.toFixed(1) + '"' + (d.activated?"":"disabled") +  '></input> ' +  d.spent.toFixed(1) +
-      '<span class="fixed" style="visibility:' + ((d.fixed & d.activated)?"visible":"hidden") + '">&#128274</span>' + 
-      '</td><td><input class="hour_note" type="textarea" ' + (d.activated?"":"disabled") + 'value="' +d.Note + '"></td>';
-      return(html);
-      }
-      function handle_select(dat, ind) {
-      dat.activated = d3.select(this).property("checked");
-      dat.fixed = !!dat.default_time;
-      dat.Hours = dat.default_time || 0;
-      recalc();
-      }
-      function handle_input(dat, ind) {
-      dat.fixed = true;
-      dat.Hours = +this.value;
-      recalc();
-      }
-      function handle_increment(dat, ind) {
-      dati=d3.select("#projects").selectAll("tr").data()[ind]
-      dati.Hours = dati.Hours + (dati.default_time || 1) * (d3.event.shiftKey?-1:1);
-      dati.fixed = true;
-      recalc();
-      }
-      function handle_note(dat, ind) {
-      dat.Note = this.value;
-      recalc()
-      }
-      function recalc() {
-      var rows = d3.select("#projects").selectAll("tr");
-      var used = rows.data().filter(proj => proj.activated & proj.fixed ).reduce((h,proj)  => h+proj.Hours, 0);
-      var n_freefloat = rows.data().filter(proj => proj.activated & !proj.fixed).length;
-      var prop = Math.max(0, (n_freefloat > 0)?(db_state.hours_needed - used)/n_freefloat:0);
-      rows.each(proj => proj.Hours = proj.activated?(proj.fixed?proj.Hours:prop):(proj.default_time || 0));
-      rows.classed("text-muted", d => !d.activated);
-      rows.select("td input.hour_select")
-      .property('checked', d => d.activated);
-      rows.select("td input.hour_input")
-      .property("value", d => d.Hours.toFixed(1))
-      .attr("disabled", d => d.activated?null:"true");
-      rows.select("td input.hour_note")
-      .property("value", d => d.Note)
-      .attr("disabled", d => d.activated?null:"true");
-      rows.select("td span.fixed").style("visibility", d => (d.fixed & d.activated)?"visible":"hidden");
-      let total_hours = rows.data().reduce((tot,cur) => tot + (cur.activated?cur.Hours:0),0)
-      d3.select("#totalhours").text(total_hours.toFixed(1));
-      d3.selectAll("button")
-      .classed('btn-success', Math.abs(db_state.hours_needed - total_hours) <= 0.01)
-      .classed('btn-danger', db_state.hours_needed - total_hours > .01)
-      .classed('btn-warning', db_state.hours_needed - total_hours < -.01);
-      rows.classed("bg-warning", proj => proj.Hours.toFixed(1)!=proj.orig_hours.toFixed(1) || proj.Note!=proj.orig_note || proj.activated != proj.orig_activated); 
-      unsaved = d3.selectAll("tr.bg-warning").size()!=0;
-      }
-      function unloadPage(){
-      if(unsaved){
-      return "You have unsaved changes on this page. Do you want to leave this page and discard your changes or stay on this page?";
-      }
-      }
-      
-      window.onbeforeunload = unloadPage;
+     function parse_time_label(php_response) {
+	 if (php_response.hasOwnProperty('day')) {
+	     let d = d3.timeParse("%Y-%m-%d")(php_response.day);
+	     db_state.hours_needed=hours_per_day;
+	     return(d3.timeFormat("%a %e %b %Y")(d));
+	 }
+	 if (php_response.hasOwnProperty('week')) {
+	     let d = d3.timeParse("%GW%V")(php_response.week);
+	     db_state.hours_needed=hours_per_day * 5;
+	     return(d3.timeFormat("week beginning %a %e %b %Y")(d));
+	 }
+	 if (php_response.hasOwnProperty('month')) {
+	     let d = d3.timeParse("%GW%V")(php_response.month);
+	     db_state.hours_needed=hours_per_day * 0;
+	     return(d3.timeFormat("%B %Y")(d));
+	 }
+	 
+     }
+     function movetime(forward) {
+	 var cur = new Date(db_state.start);
+	 var nxt;
+	 db_state.move = (forward)?"forward":"reverse";
+	 if (!unsaved || confirm("You're navigating away from unsaved changes")) {
+	     get_timesheet(db_state);
+	 }
+     }
+     function generate_row_from_data(d,i) {
+	 var html = '<td><input type="checkbox" class="form-check-input hour hour_select" autocomplete="off" ' + (d.activated?"checked":"") + '>' + 
+		    '<span class="hour_increment">' + d.Project + (d.codekey?(" ("+d.codekey+")"):"") + '</span>' +
+		    "</td><td>" + d.Scientist +
+		    "</td><td>" + d.Lab +
+		    '</td><td><input size="5" type="number" min="0" max="168" step="' + (0.1) +'" class="hour hour_input" value="' + d.Hours.toFixed(1) + '"' + (d.activated?"":"disabled") +  '></input> ' +
+		    '<span class="spent">' + d.spent.toFixed(1) + '</span>' + 
+		    '<span class="fixed" style="visibility:' + ((d.fixed & d.activated)?"visible":"hidden") + '">&#128274</span>' + 
+		    '</td><td><input class="hour_note" type="textarea" ' + (d.activated?"":"disabled") + 'value="' +d.Note + '"></td>';
+	 return(html);
+     }
+     function handle_select(dat, ind) {
+	 dat.activated = d3.select(this).property("checked");
+	 dat.fixed = !!dat.default_time;
+	 dat.Hours = dat.default_time || 0;
+	 recalc();
+     }
+     function handle_input(dat, ind) {
+	 dat.fixed = true;
+	 dat.Hours = +this.value;
+	 recalc();
+     }
+     function handle_increment(dat, ind) {
+	 dati=d3.select("#projects").selectAll("tr").data()[ind]
+	 dati.Hours = dati.Hours + (dati.default_time || 1) * (d3.event.shiftKey?-1:1);
+	 dati.fixed = true;
+	 recalc();
+     }
+     function handle_note(dat, ind) {
+	 dat.Note = this.value;
+	 recalc()
+     }
+     function recalc() {
+	 var rows = d3.select("#projects").selectAll("tr");
+	 var used = rows.data().filter(proj => proj.activated & proj.fixed ).reduce((h,proj)  => h+proj.Hours, 0);
+	 var n_freefloat = rows.data().filter(proj => proj.activated & !proj.fixed).length;
+	 var prop = Math.max(0, (n_freefloat > 0)?(db_state.hours_needed - used)/n_freefloat:0);
+	 rows.each(proj => proj.Hours = proj.activated?(proj.fixed?proj.Hours:prop):(proj.default_time || 0));
+	 rows.classed("text-muted", d => !d.activated);
+	 rows.select("td input.hour_select")
+	     .property('checked', d => d.activated);
+	 rows.select("td input.hour_input")
+	     .property("value", d => d.Hours.toFixed(1))
+	     .attr("disabled", d => d.activated?null:"true");
+	 rows.select("td span.spent")
+	     .text(d => d.spent.toFixed(1));
+	 rows.select("td input.hour_note")
+	  .property("value", d => d.Note)
+	     .attr("disabled", d => d.activated?null:"true");
+	 rows.select("td span.fixed").style("visibility", d => (d.fixed & d.activated)?"visible":"hidden");
+	 let total_hours = rows.data().reduce((tot,cur) => tot + (cur.activated?cur.Hours:0),0)
+	 d3.select("#totalhours").text(total_hours.toFixed(1));
+	 d3.selectAll("button")
+	   .classed('btn-success', Math.abs(db_state.hours_needed - total_hours) <= 0.01)
+	   .classed('btn-danger', db_state.hours_needed - total_hours > .01)
+	   .classed('btn-warning', db_state.hours_needed - total_hours < -.01);
+	 rows.classed("bg-warning", proj => proj.Hours.toFixed(1)!=proj.orig_hours.toFixed(1) || proj.Note!=proj.orig_note || proj.activated != proj.orig_activated); 
+	 unsaved = d3.selectAll("tr.bg-warning").size()!=0;
+     }
+     function unloadPage(){
+	 if(unsaved){
+	     return "You have unsaved changes on this page. Do you want to leave this page and discard your changes or stay on this page?";
+	 }
+     }
+     
+     window.onbeforeunload = unloadPage;
     </script>
   </body>
 </html>
