bdd31d3ea9bba78a24bcd4ad3d25ea036126660d
Gavin Kelly <gavin.kelly@crick.ac.uk>
2020-11-08 20:34:00 +0000

    Add existing generic projects

---
+++ yml/babs.js
@@ -0,0 +1,45 @@
+babs=[
+    {
+	Project: "Annual Leave",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 7.2,
+	Active: "True"
+    },
+    {
+	Project: "Bank Holiday",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 7.2,
+	Active: "True"
+    },
+    {
+	Project: "Lab Meeting",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 2.5,
+	Active: "True"
+    },
+    {
+	Project: "Seminar",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 1,
+	Active: "True"
+    },
+    {
+	Project: "Admin",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 1,
+	Active: "True"
+    },
+    {
+	Project: "Training - Giving",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 1,
+	Active: "True"
+    },
+    {
+	Project: "Staff Management",
+	Scientist: "aengus.stewart@crick.ac.uk",
+	default_time: 1,
+	Active: "True"
+    }
+    ]
+    
